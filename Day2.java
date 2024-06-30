cf66855a126ff1ac22a817495456f368c19bd808
lasyahari6 <lasyakanchimireddy6@gmail.com>
2024-06-30 10:08:06 +0530

    First Commit

---
+++ Day2.java
@@ -0,0 +1,21 @@
+/*calculate the meals cost by adding tip and tax percentage*/
+import java.util.*;
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);     /*getting input from user*/
+        double mealcost=scan.nextDouble();
+        int tip_percent=scan.nextInt();
+        int tax_percent=scan.nextInt();
+        double tip=(mealcost/100)*tip_percent;  /*calculate tip and tax % */
+        double tax=(mealcost/100)*tax_percent;   
+        double total=mealcost+tax+tip;  /*calculate total */
+        int value=(int)Math.round(total);   /*roundof total cost */
+        System.out.println(tip);
+        System.out.println(tax);
+        System.out.println(total);
+        System.out.println(value);
+        scan.close();
+
+    }
+    
+}
--- Practice.java
+++ Practice.java
@@ -2,23 +2,8 @@
 import java.util.*;
 class Practice{
     public static void main(String[] args) {
-    int i = 4;
-    double d = 4.0;
-    String s = "HackerRank ";
+    int i = 4, j=9;
+    System.out.println(i/j);
 
-    Scanner scan = new Scanner(System.in);
-
-    int number;
-    double doubleNumber;
-    String text;
-    doubleNumber= scan.nextDouble();
-    scan.nextLine();
-    text= scan.nextLine();
-    number= scan.nextInt();
-    System.out.println(i + number);
-    System.out.println(d + doubleNumber);
-    System.out.println(s+ text);
-
-    scan.close();
     }
 }
