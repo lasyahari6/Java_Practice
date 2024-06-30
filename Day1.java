5b0fa55d111f2a442e582945fc0f21de10d48c17
lasyahari6 <lasyakanchimireddy6@gmail.com>
2024-06-30 09:11:29 +0530

    First Commit

--- Day1.java
+++ Day1.java
@@ -1,8 +1,4 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Day1 {
 	
@@ -10,31 +6,14 @@ public class Day1 {
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
-		
         Scanner scan = new Scanner(System.in);
-
-        /* Declare second integer, double, and String variables. */
         int i1=scan.nextInt();
         double d1=scan.nextDouble();
         scan.nextLine();
         String s1=scan.next();
-
-
-        /* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-
-        /* Print the sum of both integer variables on a new line. */
         System.out.println(i1+i);
         System.out.println(d1+d);
-        System.out.print(s);
-        System.out.println(s1);
-
-
-        /* Print the sum of the double variables on a new line. */
-		
-        /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
-
+        System.out.println(s+s1);
         scan.close();
     }
 }
