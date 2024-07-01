a5b87af26046ee093096827337e66a0333916028
lasyahari6 <137754841+lasyahari6@users.noreply.github.com>
2024-07-01 18:39:08 +0530

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -1,3 +1,4 @@
+/*Anagrams or not*/
 import java.util.*;
 import java.util.Arrays;
 public class Anagrams {
@@ -6,15 +7,15 @@ public class Anagrams {
         String s1, s2;
         s1=scan.nextLine();
         s2=scan.nextLine();
-        String str1=s1.toLowerCase();
+        String str1=s1.toLowerCase();             /*convert string to lower case values*/
         String str2=s2.toLowerCase();
-        if(s1.length()==s2.length()){
-            char[] ch1=str1.toCharArray();
+        if(s1.length()==s2.length()){             /*check for length of string*/
+            char[] ch1=str1.toCharArray();        /*changing string to characters and store in arrays*/
             char[] ch2=str2.toCharArray();
             
-            Arrays.sort(ch1);
+            Arrays.sort(ch1);                    /*sort characters in array*/
             Arrays.sort(ch2);
-            Boolean result =Arrays.equals(ch1,ch2);
+            Boolean result =Arrays.equals(ch1,ch2);    /*check whether the arrays are equal or not*/
             if(result)System.out.println("Anagrams");
             else System.out.println("Not Anagrams");
         }
